266c267546b6af5f78cab301509f9224817c3e79
d4cho <danielcho91@gmail.com>
2022-03-15 16:32:43 -0400

    remove test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Wordle', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Wordle/i);
-    expect(linkElement).toBeInTheDocument();
-});
